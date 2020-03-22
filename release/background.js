f46c78eddb9e5c873b5bf1aaa2806062f878ab0f
Maksim Daunarovich <max.daunarovich@gmail.com>
2020-03-22 23:20:57 +0300

    Add message processing and storing value into extension storage (#3)

--- release/background.js
+++ release/background.js
@@ -1,7 +1,28 @@
 chrome.runtime.onMessage.addListener((message, sender, senderResponse) => {
-  console.log(message);
   if (message.msg === "get-logo-image") {
     const logoSrc = chrome.runtime.getURL("flow-logo-dark-theme.svg");
     senderResponse({ link: logoSrc, req: "flow-logo-dark-theme" });
+  }
+
+  if (message.msg === "store-theme-selection") {
+    const { theme } = message;
+    chrome.storage.sync.set({ flowTheme: theme });
+    senderResponse({});
+  }
+
+  if (message.msg === "get-theme") {
+    chrome.storage.sync.get(["flowTheme"], result => {
+      const { flowTheme } = result;
+      if (!flowTheme) {
+        chrome.storage.sync.set({ flowTheme: "light" }, () => {
+          senderResponse({ flowTheme: "light" });
+        });
+      } else {
+        senderResponse({ flowTheme: flowTheme });
+      }
+    });
+
+    // set this to true in order to make message processing async
+    return true;
   }
 });
