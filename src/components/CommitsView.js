b8bb1c3201ea0e1d06b4d65e1e2bb7c74aa9423f
Max Daunarovich <max.daunarovich@gmail.com>
2020-04-27 00:51:04 +0300

    Create views for file selector

--- src/components/CommitsView.js
+++ src/components/CommitsView.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
+import Select from "react-select";
 import Commit from "./Commit";
 import {
   CommitsContainer,
   MainContainer,
   SectionHeader,
+  SelectContainer,
   Title,
   Empty,
 } from "./styles";
@@ -34,8 +36,13 @@ class CommitsView extends Component {
 
   render() {
     const { commitList, router, fileManager } = this.props;
-    const { loading, createNew, cadenceFiles } = commitList;
-    const { filename } = fileManager;
+    const {
+      loading,
+      createNew,
+      fileList,
+      fileListGroups,
+      defaultValue,
+    } = commitList;
     return (
       <MainContainer>
         <a id="gh-copy-code-injector" style={{ display: "none" }} />
@@ -53,25 +60,21 @@ class CommitsView extends Component {
             }}
           />
         </SectionHeader>
-        {cadenceFiles.length > 0 && (
-          <div>
-            <p>{filename}</p>
-            <select
-              value={filename}
-              onChange={(event) => {
-                const newFile = event.target.value;
-                fileManager.updateFilename(newFile);
+        {fileList.length > 0 && (
+          <SelectContainer>
+            <Select
+              value={{
+                value: fileManager.filename,
+                label: fileManager.filename,
               }}
-            >
-              {cadenceFiles.map((file) => {
-                return (
-                  <option key={file.name} value={file.name}>
-                    {file.name}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
+              isSearchable={true}
+              options={fileListGroups}
+              width={"100%"}
+              onChange={({ value }) => {
+                fileManager.updateFilename(value);
+              }}
+            />
+          </SelectContainer>
         )}
         <NewCommit
           createNew={createNew}
